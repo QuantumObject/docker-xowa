be167577e5d6a92c6378569e742a3daa6f2f4bcf
Angel Rodriguez-Ricardo <angelrr7702@gmail.com>
2020-01-19 19:14:01 -0500

    update to Version 4.6.5.1911

--- pre-conf.sh
+++ pre-conf.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-xowa_version=v4.6.4.1908
+xowa_version=v4.6.5.1911
 mkdir -p /opt/xowa
 cd /opt/xowa
 wget https://github.com/gnosygnu/xowa/releases/download/${xowa_version}/xowa_app_linux_64_${xowa_version}.zip
